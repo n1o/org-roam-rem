eae261a686ef94f360b60994c83555dfa8676f97
marek barak <mrk.barak@gmail.com>
2021-09-09 18:30:14 +0200

    Org ani stuff

--- org-roam-rem.el
+++ org-roam-rem.el
@@ -23,12 +23,21 @@
 (require 'org-element)
 (require 'org-roam)
 (require 'subr-x)
+(require 'request)
+(require 'ox-html)
+(require 'ox)
+(require 'json)
 
 (defconst org-roam-rem-card-levels "REM_LEVELS")
 (defconst org-roam-rem-card-title "CARD_TITLE")
 
-(defcustom org-roam-rem-roam-title-in-card-title t "Include parent node title in card title")
-(defcustom org-roam-rem-ancestor-path-card-title t "Include ancestor path in card title")
+(defcustom org-roam-rem-roam-title-in-card-title t "Include parent node title in card title.")
+(defcustom org-roam-rem-ancestor-path-card-title t "Include ancestor path in card title.")
+(defcustom org-roam-rem-anki-connect-listening-address "127.0.0.1" "The network address AnkiConnect is listening.")
+(defcustom org-roam-rem-anki-connect-listening-port "8765" "The port number AnkiConnect is listening.")
+(defcustom org-roam-rem-break-consecutive-braces-in-latex nil "If non-nil, consecutive `}' will be automatically separated by spaces to prevent early-closing of cloze.
+See https://apps.ankiweb.net/docs/manual.html#latex-conflicts.")
+(defcustom org-roam-rem-use-math-jax nil "Use Anki's built in MathJax support instead of LaTeX.")
 
 (defgroup org-roam-rem nil
   "Customizations for org-roam."
@@ -136,5 +145,232 @@
                      (let ((sibling (org-element-at-point)))
                        (push (org-roam-rem--card-exlcusion-from-node sibling) exclusion))))))))
     exclusion))
+
+(defun org-roam-rem--anki-connect-action (action &optional params version)
+  (let (a)
+    (when version
+      (push `(version . ,version) a))
+    (when params
+      (push `(params . ,params) a))
+    (push `(action . ,action) a)))
+
+(defun org-roam-rem--anki-connect-invoke-queue ()
+  (let (action-queue)
+    (lambda (&optional action params handler)
+      (if action
+          (push (cons (org-roam-rem--anki-connect-action action params) handler) action-queue)
+        (when action-queue
+          (apply #'org-roam-rem--anki-connect-invoke-multi (nreverse action-queue))
+          (setq action-queue nil))))))
+
+(defun org-roam-rem--anki-connect-invoke (action &optional params)
+  "Invoke AnkiConnect with ACTION and PARAMS."
+  (let ((request-body (json-encode (org-roam-rem--anki-connect-action action params 5)))
+        (request-backend 'curl)
+        (json-array-type 'list)
+        reply err)
+
+    (let ((response (request (format "http://%s:%s"
+                                     org-roam-rem-anki-connect-listening-address
+                                     org-roam-rem-anki-connect-listening-port)
+                             :type "POST"
+                             :parser 'json-read
+                             :data request-body
+                             :success (cl-function (lambda (&key data &allow-other-keys)
+                                                     (setq reply data)))
+                             :error (cl-function (lambda (&key _ &key error-thrown &allow-other-keys)
+                                                   (setq err (string-trim (cdr error-thrown)))))
+                             :sync t)))
+
+      ;; HACK: With sync set to t, `request' waits for curl process to
+      ;; exit, then response data becomes available, but callbacks
+      ;; might not be called right away but at a later time, that's
+      ;; why here we manually invoke callbacks to receive the result.
+      (unless (request-response-done-p response)
+        (request--curl-callback (get-buffer-process (request-response--buffer response)) "finished\n")))
+
+    (when err (error "Error communicating with AnkiConnect using cURL: %s" err))
+    (or reply (error "Got empty reply from AnkiConnect"))))
+
+(defun org-roam-rem--anki-connect-store-media-file (path)
+  "Store media file for PATH, which is an absolute file name.
+The result is the path to the newly stored media file."
+  (let* ((hash (secure-hash 'sha1 path))
+         (media-file-name (format "%s-%s%s"
+                                  (file-name-base path)
+                                  hash
+                                  (file-name-extension path t)))
+         content)
+    (when (equal :json-false (org-roam-rem--anki-connect-invoke-result
+                              "retrieveMediaFile"
+                              `((filename . ,media-file-name))))
+      (message "Storing media file to Anki for %s..." path)
+      (setq content (base64-encode-string
+		     (with-temp-buffer
+		       (insert-file-contents path)
+		       (buffer-string))))
+      (org-roam-rem--anki-connect-invoke-result
+       "storeMediaFile"
+       `((filename . ,media-file-name)
+         (data . ,content))))
+    media-file-name))
+
+(defconst org-roam-rem--ox-anki-html-backend
+  (if org-roam-rem-use-math-jax
+      (org-export-create-backend
+       :parent 'html
+       :transcoders '((latex-fragment . org-roam-rem--ox-latex-for-mathjax)
+                      (latex-environment . org-roam-rem--ox-latex-for-mathjax)))
+    (org-export-create-backend
+     :parent 'html
+     :transcoders '((latex-fragment . org-roam-rem--ox-latex)
+                    (latex-environment . org-roam-rem--ox-latex)))))
+
+(defconst org-roam-rem--ox-export-ext-plist
+  '(:with-toc nil :org-roam-rem-mode t))
+
+(defun org-roam-rem--translate-latex-delimiters (latex-code)
+  (catch 'done
+    (let ((delimiter-map (list (list (cons (format "^%s" (regexp-quote "$$")) "[$$]")
+                                     (cons (format "%s$" (regexp-quote "$$")) "[/$$]"))
+                               (list (cons (format "^%s" (regexp-quote "$")) "[$]")
+                                     (cons (format "%s$" (regexp-quote "$")) "[/$]"))
+                               (list (cons (format "^%s" (regexp-quote "\\(")) "[$]")
+                                     (cons (format "%s$" (regexp-quote "\\)")) "[/$]"))
+                               (list (cons (format "^%s" (regexp-quote "\\[")) "[$$]")
+                                     (cons (format "%s$" (regexp-quote "\\]")) "[/$$]"))))
+          (matched nil))
+      (save-match-data
+        (dolist (pair delimiter-map)
+          (dolist (delimiter pair)
+            (when (setq matched (string-match (car delimiter) latex-code))
+              (setq latex-code (replace-match (cdr delimiter) t t latex-code))))
+          (when matched (throw 'done latex-code)))))
+    latex-code))
+
+(defun org-roam-rem--translate-latex-delimiters-to-anki-mathjax-delimiters (latex-code)
+  (catch 'done
+    (let ((delimiter-map (list (list (cons (format "^%s" (regexp-quote "$$")) "\\[")
+                                     (cons (format "%s$" (regexp-quote "$$")) "\\]"))
+                               (list (cons (format "^%s" (regexp-quote "$")) "\\(")
+                                     (cons (format "%s$" (regexp-quote "$")) "\\)"))))
+          (matched nil))
+      (save-match-data
+        (dolist (pair delimiter-map)
+          (dolist (delimiter pair)
+            (when (setq matched (string-match (car delimiter) latex-code))
+              (setq latex-code (replace-match (cdr delimiter) t t latex-code))))
+          (when matched (throw 'done latex-code)))))
+    latex-code))
+
+(defun org-roam-rem--wrap-latex (content)
+  "Wrap CONTENT with Anki-style latex markers."
+  (format "<p><div>[latex]</div>%s<div>[/latex]</div></p>" content))
+
+(defun org-roam-rem--wrap-latex-for-mathjax (content)
+  "Wrap CONTENT for Anki's native MathJax support."
+  (format "<p>%s</p>" content))
+
+(defun org-roam-rem--wrap-div (content)
+  (format "<div>%s</div>" content))
+
+(defun org-roam-rem--ox-latex (latex _contents _info)
+  "Transcode LATEX from Org to HTML.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (let ((code (org-remove-indentation (org-element-property :value latex))))
+    (setq code
+          (pcase (org-element-type latex)
+            ('latex-fragment (org-roam-rem--translate-latex-delimiters code))
+            ('latex-environment (org-roam-rem--wrap-latex
+                                 (mapconcat #'org-roam-rem--wrap-div
+                                            (split-string (org-html-encode-plain-text code) "\n")
+                                            "")))))
+
+    (if org-roam-rem-break-consecutive-braces-in-latex
+        (replace-regexp-in-string "}}" "} } " code)
+      code)))
+
+(defun org-roam-rem--ox-latex-for-mathjax (latex _contents _info)
+  "Transcode LATEX from Org to HTML.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (let ((code (org-remove-indentation (org-element-property :value latex))))
+    (setq code
+          (pcase (org-element-type latex)
+            ('latex-fragment (org-roam-rem--translate-latex-delimiters-to-anki-mathjax-delimiters code))
+            ('latex-environment (org-roam-rem--wrap-latex-for-mathjax
+                                 (mapconcat #'org-roam-rem--wrap-div
+                                            (split-string (org-html-encode-plain-text code) "\n")
+                                            "")))))
+
+    (if org-roam-rem-break-consecutive-braces-in-latex
+        (replace-regexp-in-string "}}" "} } " code)
+      code)))
+
+(defun org-roam-rem--ox-html-link (oldfun link desc info)
+  "When LINK is a link to local file, transcodes it to html and stores the target file to Anki, otherwise calls OLDFUN for help.
+The implementation is borrowed and simplified from ox-html."
+
+  (or (catch 'giveup
+        (unless (plist-get info :org-roam-rem-mode)
+          (throw 'giveup nil))
+
+        (let* ((type (org-element-property :type link))
+               (raw-path (org-element-property :path link))
+               (desc (org-string-nw-p desc))
+               (path
+                (cond
+                 ((string= type "file")
+                  ;; Possibly append `:html-link-home' to relative file
+                  ;; name.
+                  (let ((inhibit-message nil)
+                        (home (and (plist-get info :html-link-home)
+                                   (org-trim (plist-get info :html-link-home)))))
+                    (when (and home
+                               (plist-get info :html-link-use-abs-url)
+                               (file-name-absolute-p raw-path))
+                      (setq raw-path (concat (file-name-as-directory home) raw-path)))
+                    ;; storing file to Anki and return the modified path
+                    (org-roam-rem--anki-connect-store-media-file (expand-file-name (url-unhex-string raw-path)))))
+                 (t (throw 'giveup nil))))
+               (attributes-plist
+                (let* ((parent (org-export-get-parent-element link))
+                       (link (let ((container (org-export-get-parent link)))
+                               (if (and (eq (org-element-type container) 'link)
+                                        (org-html-inline-image-p link info))
+                                   container
+                                 link))))
+                  (and (eq (org-element-map parent 'link 'identity info t) link)
+                       (org-export-read-attribute :attr_html parent))))
+               (attributes
+                (let ((attr (org-html--make-attribute-string attributes-plist)))
+                  (if (org-string-nw-p attr) (concat " " attr) ""))))
+          (cond
+           ;; Image file.
+           ((and (plist-get info :html-inline-images)
+                 (org-export-inline-image-p
+                  link (plist-get info :html-inline-image-rules)))
+            (org-html--format-image path attributes-plist info))
+
+           ;; Audio file.
+           ((string-suffix-p ".mp3" path t)
+              (format "[sound:%s]" path))
+
+           ;; External link with a description part.
+           ((and path desc) (format "<a href=\"%s\"%s>%s</a>"
+                                    (org-html-encode-plain-text path)
+                                    attributes
+                                    desc))
+
+           ;; External link without a description part.
+           (path (let ((path (org-html-encode-plain-text path)))
+                   (format "<a href=\"%s\"%s>%s</a>"
+                           path
+                           attributes
+                           (org-link-unescape path))))
+
+           (t (throw 'giveup nil)))))
+      (funcall oldfun link desc info)))
+
+
 (provide 'org-roam-rem)
 ;;; org-roam-rem.el ends here
